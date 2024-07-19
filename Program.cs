b314a156a7b3792e98360db1753c7ca84ff24a31
Mantvydas Šemeta <mantvydassemeta@gmail.com>
2024-07-19 17:05:04 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Buffers;
+using System.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ManoPrograma
+{
+    public class MokomesMasyvus
+    {
+
+        //public static void Main(string[] args)
+        //{
+        //    //int[] masyvas = { 1, 2, 3, 4, 5 };
+        //    int[] masyvas = { 6, 222, 3, 8, 543, 2, 90, 17, 84 };
+        //    //int[] masyvas = { 0, 0, 0, 0, 5, 0, 0, 0, 0 };
+        //    SpausdintiMasyva(masyvas);
+
+
+        //}
+        ////1 uzduotis
+
+        //public static void SpausdintiMasyva(int[] m)
+        //{
+        //    Console.WriteLine(m[0]);
+        //}
+
+        //2 uzduotis
+
+        //public static void SpausdintiMasyva(int[] m)
+        //{
+        //    m[1] = 2;
+        //    Console.WriteLine(m[1]);
+        //}
+
+        //3 uzduotis
+
+        //public static void SpausdintiMasyva(int[] m)
+        //{
+        //    Console.WriteLine(m.Length);
+        //}
+
+        //4 uzduotis
+
+        //public static void SpausdintiMasyva(int[] m)
+        //{
+        //    Console.WriteLine(m[2]);
+        //}
+
+        //5 uzduotis
+
+        //public static void SpausdintiMasyva(int[] m)
+        //{
+        //    Console.WriteLine(m.Last());
+        //}
+
+        //6 uzduotis
+
+        //public static void SpausdintiMasyva(int[] m)
+        //{
+        //        Console.WriteLine(m[m.Length / 2]);
+        //}
+
+        //7 uzduotis
+
+        //public static void SpausdintiMasyva(int[] m)
+        //{
+        //    Console.WriteLine(m.Sum());
+        //}
+
+        //8 uzduotis
+
+        //public static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Patikrink ar skaičius yra masyve: ");
+        //    int[] masyvas = { 6, 222, 3, 8, 543, 2, 90, 17, 84 };
+        //    ArYra(masyvas);
+
+        //}
+        //public static void ArYra(int[] m)
+        //{
+
+        //    if (m.Contains(int.Parse(Console.ReadLine())))
+        //    {
+        //        Console.WriteLine("Skaicius yra masyve");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Skaiciaus nera masyve");
+        //    }
+        //}
+
+        //9 uzduotis
+
+        //public static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Ivesk indeksa: ");
+        //    double[] masyvas = {2.66, 2.5, 5.66, 8.44, 5.6, 2.7, 8.6, 0.6, 8.4 };
+        //    int index = int.Parse(Console.ReadLine());
+        //    ArTinkamasIndeksas(masyvas, index);
+        //}
+        //public static void ArTinkamasIndeksas(double[] m, int i)
+        //{
+        //    if (i <= m.Length)
+        //            {
+        //      m[i] = 3;
+        //        Console.WriteLine(m[i]);
+
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Tokio indekso nera");
+        //    }
+
+        //10 uzduotis
+
+        //public static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Ivesk raide: ");
+        //    char[] masyvas = { 'a', 'b', 'i', 'w', 'r' };
+        //    ArRaide(masyvas);
+        //}
+        //public static void ArRaide(char[] m)
+        //{
+        //    if (m.Contains(char.Parse(Console.ReadLine())))
+        //    {
+        //        Console.WriteLine("Raide yra masyve");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Raides nera masyve");
+        //    }
+        //}
+
+        //11 uzduotis
+
+        //public static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Ivesk zodi: ");
+        //    string[] masyvas = { "mama", "tetis", "brolis", "sese", "teta" };
+        //    ArZodis(masyvas);
+        //}
+        //public static void ArZodis(string[] m)
+        //{
+        //    if (m.Contains((Console.ReadLine())))
+        //    {
+        //        Console.WriteLine("Zodis yra masyve");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Zodzio nera masyve");
+        //    }
+        //}
+
+        //12 uzduotis
+        //public static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Ivesk pirma skaiciu: ");
+        //    int pirmas = int.Parse(Console.ReadLine());
+        //    Console.WriteLine("Ivesk antra skaiciu: ");
+        //    int suma = pirmas + int.Parse(Console.ReadLine());
+        //    int[] masyvas = { 1, 2, 3, 4, 5 };
+        //    AtitinkaSuma(masyvas, suma);
+
+        //}
+        //public static void AtitinkaSuma(int[] m, int s)
+        //{
+        //    if (m.Contains(s))
+        //    {
+        //        Console.WriteLine("Skaiciu suma atitinka viena masyvo reiksme");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Skaiciu suma neatitinka nei vienos masyvo reiksmes");
+        //    }
+        //}
+
+
+
+
+
+
+    }
+
+
+}
+
+
+
+
